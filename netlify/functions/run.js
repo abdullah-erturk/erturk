e27bdcb9fd15ac013cf66c23536f5d5885d0d5ba
Abdullah ERTÜRK <54866587+abdullah-erturk@users.noreply.github.com>
2025-10-10 21:48:50 +0300

    Update run.js

--- netlify/functions/run.js
+++ netlify/functions/run.js
@@ -59,7 +59,7 @@ catch {
 <title>erturk</title>
 </head>
 <body>
-<section style="font-family: Consolas, monospace; background: #1e1e1e; color: #e4e4e4; padding: 20px; border-radius: 10px; max-width: 700px;">
+<section style="font-family: Consolas, monospace; background: #1e1e1e; color: #e4e4e4; padding: 20px; border-radius: 10px; max-width: 800px;">
   <h2 style="color: #4fc3f7;">💻 PowerShell Komut Satırı Üzerinden Çalıştırmak İçin:</h2>
   <ol style="margin-left: 20px;">
     <li>PowerShell'i açın.<br>
